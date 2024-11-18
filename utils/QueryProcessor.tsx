66def2293085743204ec23e8bd4246a515be436b
Jeam Montiel <19-10234@usb.ve>
2024-11-18 14:57:42 -0400

    Add support for subtraction and extended prime number queries in QueryProcessor

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -2,8 +2,9 @@ export default function QueryProcessor(query: string): string {
   const pattern = /What is (\d+) plus (\d+)/i;
   const patternLargest = /Which of the following numbers is the largest: (\d+), (\d+), (\d+)/i;
   const patternMulti = /What is (\d+) multiplied by (\d+)/i;
-  const patternPrime = /Which of the following numbers are primes: (\d+), (\d+), (\d+)/i;
+  const patternPrime = /Which of the following numbers are primes: (\d+), (\d+), (\d+), (\d+), (\d+)/i;
   const bothcubeandsquare = /Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/i;
+  const diff = /What is (\d+) minus (\d+)/i;
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
@@ -66,5 +67,35 @@ export default function QueryProcessor(query: string): string {
       }).join(", ");
     }
   }
+
+  if(query.match(diff)) {
+    const matches = query.match(diff);
+    if (matches) {
+      const num1 = parseInt(matches[1]);
+      const num2 = parseInt(matches[2]);
+      return (num1 - num2).toString();
+    }
+  }
+
+
+  if(query.match(patternPrime)) {
+    const matches = query.match(patternPrime);
+    if (matches) {
+      const num1 = parseInt(matches[1]);
+      const num2 = parseInt(matches[2]);
+      const num3 = parseInt(matches[3]);
+      const num4 = parseInt(matches[4]);
+      const num5 = parseInt(matches[5]);
+      return [num1, num2, num3, num4, num5].filter((num) => {
+        for (let i = 2; i < num; i++) {
+          if (num % i === 0) {
+            return false;
+          }
+        }
+        return num > 1;
+      }).join(", ");
+    }
+  }
+
   return "";
 }
